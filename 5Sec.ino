3e3d866480c85ded668b07712c835db372c3daf1
clopezr9 <46933189+clopezr9@users.noreply.github.com>
2019-08-20 10:53:24 -0500

    Create 5Sec.ino

---
+++ 5Sec.ino
@@ -0,0 +1,44 @@
+#define LED22 22
+#define LED23 23
+#define LED24 24
+#define INICIO 37
+#define DETENER 36
+
+void setup()
+{
+  pinMode(LED22, OUTPUT);
+  pinMode(LED23,OUTPUT);
+  pinMode(LED24, OUTPUT);
+  pinMode(INICIO, INPUT);
+  pinMode(DETENER, INPUT);
+  digitalWrite(LED22, LOW);
+  digitalWrite(LED23, LOW);
+  digitalWrite(LED24, LOW);
+  Serial.begin(9600);
+}
+
+
+void loop(){
+  if(digitalRead(INICIO)==HIGH){
+    digitalWrite(LED22, HIGH);
+    Serial.print("Led 1 encendido");
+    delay(5000);
+    digitalWrite(LED23, HIGH);
+    Serial.print("Led 2 encendido");
+    delay(5000);
+    digitalWrite(LED24, HIGH);
+    Serial.print("Led 3 encendido");
+    delay(5000);
+    }
+  if(digitalRead(DETENER)){
+    digitalWrite(LED22, LOW);
+    Serial.print("Led 1 apagado");
+    delay(5000);
+    digitalWrite(LED23, LOW);
+    Serial.print("Led 2 apagado");
+    delay(5000);
+    digitalWrite(LED24, LOW);
+    Serial.print("Led 3 APAGADO");
+    delay(5000);
+    }
+  }
