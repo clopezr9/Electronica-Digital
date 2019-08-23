0f8186b2e9193f6af24b8109b037f40141639f83
clopezr9 <46933189+clopezr9@users.noreply.github.com>
2019-08-23 13:22:10 -0500

    Update Taller3.ino

--- Taller3.ino
+++ Taller3.ino
@@ -1,8 +1,8 @@
-#define SF A0  // Pin al que va conectado el sensor
-#define LR 2   //Pin led rojo
-#define LY 3   //Pin led Amarillo
-#define LB 4   //Pin led azul
-#define LG 5   //Pin led verde
+#define SF 0  // Pin al que va conectado el sensor
+#define LR 22   //Pin led rojo
+#define LY 23   //Pin led Amarillo
+#define LB 24   //Pin led azul
+#define LG 25   //Pin led verde
 
 int FSRread;   // Para almacenar el valor del censor
 
@@ -18,18 +18,24 @@ void setup() {
 void loop() {
   FSRread = analogRead(SF);  //Lee el sensor
   
-  Serial.println(FSRread);
+  Serial.println(FSRread);   //Imprime Valor del sensor
   
   if(FSRread<=255){
-    digitalWrite(LR, HIGH);
+    digitalWrite(LR, HIGH);   //Prende el led rojo 
   } 
   else if(FSRread<=510){
-    digitalWrite(LY, HIGH);
+    digitalWrite(LR,HIGH);
+    digitalWrite(LY, HIGH);   //Prende el led amarillo
   } 
   else if(FSRread<=765 ){
-    digitalWrite(LB, HIGH);
+    digitalWrite(LR,HIGH);
+    digitalWrite(LY, HIGH);
+    digitalWrite(LB, HIGH);   //Prende el led azul
   }
   else{
-    digitalWrite(LG, HIGH);
+    digitalWrite(LR,HIGH);
+    digitalWrite(LY, HIGH);
+    digitalWrite(LB, HIGH);   
+    digitalWrite(LG, HIGH);   //Prende el led verde
   }
 }
