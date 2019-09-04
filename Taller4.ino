14fa240e47e81e7443ad8ca726372ce109111e77
ipiedrahiv <46933373+ipiedrahiv@users.noreply.github.com>
2019-09-04 12:25:35 -0500

    Update Taller4.ino

--- Taller4.ino
+++ Taller4.ino
@@ -1,24 +1,32 @@
-#define EV  22
-#define BH  23
-#define AM  24
-#define SNH 25
-#define SNL 26
-#define SW  27
+#define EV  36
+#define BH  37
+#define AA  32
+#define AB  33
+#define SNH 30
+#define SNL 28
+#define SW  31   //dice 37
+#define uSW1 27
+#define uSW2 26
+
 
 unsigned long tmp;
+unsigned int period;
 
 void setup() {
+  Serial.begin(9600);
   //Inicializacion de sensores y actuadores
   digitalWrite(EV, LOW);
   digitalWrite(BH, LOW);
-  digitalWrite(AM, LOW);
+  digitalWrite(AA, LOW);
+  digitalWrite(AB, LOW);
   digitalWrite(SNH, LOW);
   digitalWrite(SNL, LOW);
   digitalWrite(SW, LOW);
 
   pinMode(EV, OUTPUT);
   pinMode(BH, OUTPUT);
-  pinMode(AM, OUTPUT);
+  pinMode(AA, OUTPUT);
+  pinMode(AB, OUTPUT);
   pinMode(SNH, INPUT);
   pinMode(SNL, INPUT);
   pinMode(SW, INPUT);
@@ -32,26 +40,44 @@ void loop() {
      if(digitalRead(SNL)==HIGH && digitalRead(SNH)==LOW){
         digitalWrite(BH, HIGH);
         digitalWrite(EV, LOW);
-        digitalWrite(AM, LOW);
+        digitalWrite(AA, LOW);
+        digitalWrite(AB, LOW);
      }
      else if(digitalRead(SNL)==HIGH && digitalRead(SNH)==HIGH){
+         digitalWrite(BH, LOW);  //Apaga la bomba hidraulica
+         period = 30000;
         for(int i=0; i<5; i++){
           delay(10000);
-          tmp= millis();        //Para contar los segundos que debe funcionar el agitador(30 sec)
-          if(tmp <= 30000){    
-            digitalWrite(AM,HIGH);  //Inicia el agitador 
-            digitalWrite(BH, LOW);  //Apaga la bomba hidraulica
-          }else{
-            digitalWrite(AM,LOW);   //Apaga el agitador
+          tmp = millis();
+          while( (millis()-tmp) < period){
+             Serial.println(millis()); 
+             if(digitalRead(uSW2)==LOW && digitalRead(uSW1)==LOW ){
+               digitalWrite(AA,LOW);
+             digitalWrite(AB,HIGH);
+              delay(1500); 
+             }else if(digitalRead(uSW1)== HIGH){
+              Serial.println(":::");
+             digitalWrite(AA,LOW);
+             digitalWrite(AB,HIGH);
+               delay(1500); 
+           }else if(digitalRead(uSW2) == HIGH){
+            Serial.println(":)");
+              digitalWrite(AB,LOW);
+               digitalWrite(AA,HIGH);
+              delay(1500);
+           }//Inicia el agitador
           }
-          delay(30000);  //Para que funcione??? 
+               //delay(30000);
+            digitalWrite(AA,LOW);   //Apaga el agitador
+             digitalWrite(AB,LOW);
         }
         digitalWrite(EV,HIGH);//Abre electro valvula para vaciar
      }
   }else{
     digitalWrite(EV, LOW);
     digitalWrite(BH, LOW);
-    digitalWrite(AM, LOW);
+    digitalWrite(AA, LOW);
+     digitalWrite(AB, LOW);
     digitalWrite(SNH, LOW);
     digitalWrite(SNL, LOW);
     digitalWrite(SW, LOW);
